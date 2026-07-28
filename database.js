1c908c6b437103ace79009f12ea30de99ba4acad
Eclipserabot <eclipseralegends5492@gmail.com>
2026-07-28 14:02:45 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
 
-const FILE = "./invites.json";
+const FILE = "./database.json";
 
 function load() {
-    if (!fs.existsSync(FILE)) return {};
+    if (!fs.existsSync(FILE)) {
+        return {
+            invites: {},
+            pending: {},
+            messages: {}
+        };
+    }
+
     return JSON.parse(fs.readFileSync(FILE, "utf8"));
 }
 
@@ -11,33 +18,66 @@ function save(data) {
     fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
-function addInvite(userId) {
+function addPending(userId, inviterId) {
+    const data = load();
+    data.pending[userId] = inviterId;
+    data.messages[userId] = 0;
+    save(data);
+}
+
+function addMessage(userId) {
     const data = load();
 
-    if (!data[userId]) {
-        data[userId] = 0;
+    if (data.messages[userId] === undefined) return 0;
+
+    data.messages[userId]++;
+
+    save(data);
+
+    return data.messages[userId];
+}
+
+function getInviter(userId) {
+    const data = load();
+    return data.pending[userId];
+}
+
+function completeInvite(userId) {
+    const data = load();
+
+    const inviterId = data.pending[userId];
+
+    if (!inviterId) return;
+
+    if (!data.invites[inviterId]) {
+        data.invites[inviterId] = 0;
     }
 
-    data[userId]++;
+    data.invites[inviterId]++;
+
+    delete data.pending[userId];
+    delete data.messages[userId];
 
     save(data);
 }
 
 function getInvites(userId) {
     const data = load();
-    return data[userId] || 0;
+    return data.invites[userId] || 0;
 }
 
 function getLeaderboard() {
     const data = load();
 
-    return Object.entries(data)
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, 10);
+    return Object.entries(data.invites)
+        .sort((a, b) => b[1] - a[1]);
 }
 
 module.exports = {
-    addInvite,
+    addPending,
+    addMessage,
+    getInviter,
+    completeInvite,
     getInvites,
     getLeaderboard
 };
