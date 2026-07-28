ba0d8feb22d485df6f8877acef8fd188c6496786
Eclipserabot <eclipseralegends5492@gmail.com>
2026-07-28 18:15:25 +0530

    Refactor environment variable usage in deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
+const TOKEN = process.env.TOKEN || "YOUR_BOT_TOKEN";
+const CLIENT_ID = process.env.CLIENT_ID || "YOUR_CLIENT_ID";
+const GUILD_ID = process.env.GUILD_ID || "YOUR_SERVER_ID";
+
 const commands = [
     new SlashCommandBuilder()
         .setName("invites")
@@ -9,20 +13,21 @@ const commands = [
 
     new SlashCommandBuilder()
         .setName("top")
-        .setDescription("Show invite leaderboard")
+        .setDescription("Show the invite leaderboard")
 ].map(cmd => cmd.toJSON());
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
+
     try {
 
         console.log("Registering slash commands...");
 
         await rest.put(
             Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
+                CLIENT_ID,
+                GUILD_ID
             ),
             {
                 body: commands
@@ -36,4 +41,5 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
         console.error(err);
 
     }
+
 })();
