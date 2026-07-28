2f154216c689524c5cd6794d1e3b6e08fd91c1d6
Eclipserabot <eclipseralegends5492@gmail.com>
2026-07-28 08:43:36 +0530

    Refactor invite functions and add leaderboard feature

--- database.js
+++ database.js
@@ -3,26 +3,41 @@ const fs = require("fs");
 const FILE = "./invites.json";
 
 function load() {
-  if (!fs.existsSync(FILE)) return {};
-  return JSON.parse(fs.readFileSync(FILE, "utf8"));
+    if (!fs.existsSync(FILE)) return {};
+    return JSON.parse(fs.readFileSync(FILE, "utf8"));
 }
 
 function save(data) {
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
 function addInvite(userId) {
-  const data = load();
-  data[userId] = (data[userId] || 0) + 1;
-  save(data);
+    const data = load();
+
+    if (!data[userId]) {
+        data[userId] = 0;
+    }
+
+    data[userId]++;
+
+    save(data);
 }
 
 function getInvites(userId) {
-  const data = load();
-  return data[userId] || 0;
+    const data = load();
+    return data[userId] || 0;
+}
+
+function getLeaderboard() {
+    const data = load();
+
+    return Object.entries(data)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 10);
 }
 
 module.exports = {
-  addInvite,
-  getInvites,
+    addInvite,
+    getInvites,
+    getLeaderboard
 };
